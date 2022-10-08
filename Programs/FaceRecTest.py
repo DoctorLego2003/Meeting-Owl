377bd00e6a22c22963fe01cdc7b3d01292b5a5cb
Pepijn Haenen <pepijn.haenen@gmail.com>
2022-10-08 15:28:42 +0200

    Cleaning up

--- Programs/FaceRecTest.py
+++ Programs/FaceRecTest.py
@@ -13,13 +13,13 @@ for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 # Display the output
 cv2.imshow('img', img)
-gray[0,0]=5
-gray[1,1]=5
-gray[2,2]=5
-gray[3,3]=5
-gray[4,4]=5
-gray[5,5]=5
-gray[6,6]=5
-gray[7,7]=5
+#gray[0,0]=5
+#gray[1,1]=5
+#gray[2,2]=5
+#gray[3,3]=5
+#gray[4,4]=5
+#gray[5,5]=5
+#gray[6,6]=5
+#gray[7,7]=5
 cv2.imshow('gray', gray)
 cv2.waitKey()
