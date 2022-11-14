8fc83334c4f2edf87b31a4b1fc1eada2b7344ffc
arnel <arne.lemahieu@telenet.be>
2022-11-14 14:26:21 +0100

    idk

--- Programs/test_afmeting_hoofd.py
+++ Programs/test_afmeting_hoofd.py
@@ -1,27 +1,3 @@
-import cv2
-face_cascade = cv2.CascadeClassifier(r'./xml/haarcascade_frontalface_default.xml')
-
-cap = cv2.VideoCapture(0)
-
-while True:
-    ret, img = cap.read()
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_img, 1.25, 4)
-    print(faces)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-        rec_gray = gray_img[y:y + h, x:x + w]
-        rec_color = img[y:y + h, x:x + w]
-        print("breedte: ", w)
-        print("hoogte: ", h)
-        breedte = w
-        print(w)
-
-    cv2.imshow('Face Recognition', img)
-
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+lijst = {'hoofd1': 1, 'hoofd2': 2}
+lijst.pop('hoofd1')
+print(lijst)
